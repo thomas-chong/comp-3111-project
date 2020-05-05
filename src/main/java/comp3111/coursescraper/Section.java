5621e2045d826f0f0a429ed4dbd524ad91b9fa83
CHONG, Cheuk Hei <chchongaa@connect.ust.hk>
2020-05-05 10:34:57 +0800

    recovered ian-test change

--- src/main/java/comp3111/coursescraper/Section.java
+++ src/main/java/comp3111/coursescraper/Section.java
@@ -1,5 +1,8 @@
 package comp3111.coursescraper;
 
+enum timeType {AM, PM}
+enum dayType {MON, TUE, WED, THU, FRI, SAT}
+
 public class Section{
 	private static final int DEFAULT_MAX_SLOT = 10;
 	private static final int DEFAULT_MAX_INSTRUCTOR = 20;
@@ -11,13 +14,26 @@ public class Section{
 	private int numInstructor;
 	
 	public Section() {
-		for (int i = 0; i < DEFAULT_MAX_SLOT; i++) {
-			slots[i] = null;
-		}
+		slots = new Slot[DEFAULT_MAX_SLOT];
+		for (int i = 0; i < DEFAULT_MAX_SLOT; i++) slots[i] = null;
 		numSlots = 0;
-		for (int i = 0; i < DEFAULT_MAX_INSTRUCTOR; i++) {
-			instructor[i] = null;
+		instructor = new Instructor[DEFAULT_MAX_INSTRUCTOR];
+		for (int i = 0; i < DEFAULT_MAX_INSTRUCTOR; i++) instructor[i] = null;
+	}
+	
+	public Section clone() {
+		Section s = new Section();
+		s.section = this.section;
+		s.id = this.id;
+		s.numSlots = this.numSlots;
+		s.numInstructor = this.numInstructor;
+		for (int i = 0; i < s.numSlots; ++i) {
+			s.slots[i] = this.slots[i].clone();
 		}
+		for (int i = 0; i < s.numInstructor; ++i) {
+			s.instructor[i] = this.instructor[i].clone();
+		}
+		return s;
 	}
 	
 	public void setSection(String section) {
@@ -48,16 +64,61 @@ public class Section{
 		return null;
 	}
 	
-	public void addInstructor(Instructor instructor) {
-		if (numInstructor) {
-			
-		}
-		numInstructor++;
+	public int getNumSlots() {
+		return numSlots;
 	}
 	
+	public int getNumInstructors() {
+		return numInstructor;
+	}
+	
+	public Instructor getInstructor(int i) {
+		if (i >= 0 && i < numInstructor)
+			return instructor[i];
+		return null;
+	}
+	
+	public void addInstructor(Instructor in) {
+		if (numInstructor >= DEFAULT_MAX_INSTRUCTOR) {
+			return;
+		}
+		instructor[numInstructor++] = in.clone();
+	}
+	
+	/**
+	 * @param selection the target day of week
+	 * @return true if this Section that has Slot(s) on the target day of week; false otherwise
+	 */
+	public boolean hasDay(dayType selection) {
+		int selection_int = selection.ordinal();
+		for (int i = 0; i < getNumSlots(); ++i) {
+			if (selection_int == getSlot(i).getDay()) {
+				return true;
+			}
+		}
+		return false;
+	}
+	/**
+	 * @param selection the target time of day (AM/PM)
+	 * @return true if this Section has Slots in the target time of day; false otherwise
+	 */
+	public boolean hasTime(timeType selection) {
+		int selection_int = selection.ordinal();
+		if (selection_int == 0) {	// Check for slots with starting time in AM
+			//12:00 is PM
+			for (int i = 0; i < getNumSlots(); ++i) {
+				if (getSlot(i).getStartHour() < 12) {
+					return true;
+				}
+			}
+		} else {
+			for (int i = 0; i < getNumSlots(); ++i) {
+				if (getSlot(i).getEndHour() >= 12) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 	
 }
-
-
-
-
